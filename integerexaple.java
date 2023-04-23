6d52b2bc9b38b53bcec2246f795898f27b729849
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-23 21:04:18 +0530

    initial commit

---
+++ integerexaple.java
@@ -0,0 +1,6 @@
+public class integerexaple {
+    public static void main(String args[]){
+        int integerexample =10;
+         System.out.println("THE EXAMPLE OF INTEGER IS: "+integerexample);
+    }
+}
