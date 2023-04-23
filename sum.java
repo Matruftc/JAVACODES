a8243fd534affcc5b8c86dac76382358b530d35f
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-23 19:42:04 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String args[]){
+        int x=10; //choosing a number
+        int y=2;  //choosing another number
+        int z=x+y;  //addition of two number
+        System.out.println("The sum of two number is:"+z);
+    }
+}
