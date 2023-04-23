d3feabd84cbd4d019010207cdb19fdba791f1747
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-23 19:45:16 +0530

    initial commit

---
+++ subtraction.java
@@ -0,0 +1,10 @@
+public class subtraction {
+public static void main(String args[])
+{
+    int x=10; //choosing a number
+    int y=2;  //choosing another number
+    int z=x-y;  //subtraction of two number
+    System.out.println("The subtraction of two number is:"+z);
+    }
+}
+    
