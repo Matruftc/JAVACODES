3dacf984efb57a77e25add91172ae7a4e29da58f
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-23 20:48:26 +0530

    initial commit

---
+++ character.java
@@ -0,0 +1,7 @@
+public class character {
+public static void main(String args[]){
+    char charachterexample='p'; 
+    //here i take a example of character
+    System.out.println(charachterexample);
+}
+}
