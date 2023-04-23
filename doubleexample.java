1326d6b6908137525ef75534e19a7be2429727dd
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-23 20:53:19 +0530

    initial commit

---
+++ doubleexample.java
@@ -0,0 +1,6 @@
+public class doubleexample {
+    public static void main(String args[]){
+        double example_of_double=20.98;
+        System.out.println("The example of double is :"+example_of_double);
+    }
+}
