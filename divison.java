d25e994ea07432ee990111d92cfb6d50a3854f33
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-23 19:56:32 +0530

    initial commit

---
+++ divison.java
@@ -0,0 +1,13 @@
+public class divison {
+    public static void main(String args[])
+    {
+        int number1=10; //choosing a number
+        int number2=2;  //choosing another number
+        int quotient=number1/number2;  //quotient of two number
+        int remainder=number1%number2; //remainder of two number
+        System.out.println("The quotient of two number is:"+quotient);
+        System.out.println("The remainder of two number is:"+remainder);
+        }
+    }
+        
+    
