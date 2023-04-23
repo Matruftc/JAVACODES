ff2e25da2e38efb7e986cb4b1a52eef1a4b753bc
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-23 21:15:05 +0530

    initial commit

---
+++ floatexample.java
@@ -0,0 +1,6 @@
+public class floatexample {
+    public static void main(String args[]){
+    float example_of_float=20.98f;
+    System.out.println("The example of float is :"+example_of_float);
+    }
+}
