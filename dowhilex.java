52373c3e08630eca1435d48622816dd0a64f7a71
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-24 10:05:37 +0530

    initial commit

---
+++ dowhilex.java
@@ -0,0 +1,10 @@
+public class dowhilex {
+    public static void main(String args[]){
+        int number=0;
+        do{
+           System.out.println("After execution the number is: "+number);
+            number++;
+        }while(number<10);
+    }
+}
+
