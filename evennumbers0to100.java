cc3651e18696bf775bebfa274b17b5ca74a06b2c
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-24 10:26:26 +0530

    initial commit

---
+++ evennumbers0to100.java
@@ -0,0 +1,10 @@
+public class evennumbers0to100 {
+    public static void main(String args[]){
+        for(int userdefined_number=0;userdefined_number<50;userdefined_number++)
+        {
+          System.out.println("After execution the oddnumbers are: "+(2*userdefined_number));
+       
+        }   
+      
+}
+}
