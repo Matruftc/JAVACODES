60a48cd9ce10752c9add1a5eb634b7e95ebf631d
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-24 10:12:46 +0530

    initial commit

---
+++ forloopex.java
@@ -0,0 +1,8 @@
+class forloopex{
+    public static void main(String args[]){
+        int alreadytaken_number=20;
+        for(int userdefined_number=0;userdefined_number<alreadytaken_number;userdefined_number++)
+           System.out.println("After execution the number is: "+userdefined_number);
+           
+        }
+    }
