78b1f1b74a5cca7554bf3516d2babbf0166819b0
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-24 09:56:57 +0530

    initial commit

---
+++ whileloopex.java
@@ -0,0 +1,9 @@
+public class whileloopex {
+    public static void main(String args[]){
+        int number=0;
+        while(number<10){
+            System.out.println("After execution the number is: "+number);
+            number++;
+        }
+    }
+}
