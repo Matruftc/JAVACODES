2ec8b038da06df08f72052fa42adf5da64d1526a
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-27 11:21:58 +0530

    initial commit

---
+++ divison_of_arrayex.java
@@ -0,0 +1,17 @@
+class divison_of_arrayex{
+ public static void main(String []args){
+    int []array1={23,3};
+    int []array2={2,4};
+     
+    int []div=new int[2];
+ 
+    for(int i=0;i<2;i++)
+   {
+     div[i]=array1[i]/array2[i];
+     System.out.println("divison of array: "+div[i]);
+   }
+
+}}
+
+    
+
