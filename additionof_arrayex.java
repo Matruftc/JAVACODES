2289a0cdbc7bf3d46a9fcedd5af504d571b28557
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-27 11:10:41 +0530

    initial commit

---
+++ additionof_arrayex.java
@@ -0,0 +1,16 @@
+public class additionof_arrayex {
+    public static void main(String[] args) {
+ 
+     int []array1={23,3};
+     int []array2={2,4};
+     
+     int []sum=new int[2];
+ 
+    for(int i=0;i<2;i++)
+   {
+     sum[i]=array1[i]+array2[i];
+     System.out.println(sum[i]);
+   }
+}
+}
+
