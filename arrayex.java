76b057191e026532808126b0b6b8e7ef43993ac5
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-27 11:04:54 +0530

    initial commit

---
+++ arrayex.java
@@ -0,0 +1,11 @@
+public class arrayex {
+    public static void main(String[] args) {
+        int marks[]={12,12,3,13,19};
+
+        for(int initial=0;initial<marks.length;initial++)
+        {
+            System.out.println("Makrks i got secured: "+marks[initial]);
+        }
+    }
+    
+}
