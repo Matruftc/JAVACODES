99555cea056b922136c5257d828b4b6ed7566e4c
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-27 10:50:15 +0530

    initial commit

---
+++ classwithdatamemebers.java
@@ -0,0 +1,10 @@
+import javax.swing.plaf.metal.MetalBorders.ScrollPaneBorder;
+
+public class classwithdatamemebers {
+    public static void main(String[] args) {
+        int datamember1=10;
+        int datamember2=20;
+        int datamember3=datamember1+datamember2;
+        System.out.println("The result was: "+datamember3);        
+    }
+}
