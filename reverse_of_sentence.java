fc1f88cf1ab779d68ab247388e08a2deb87792bf
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-28 23:44:36 +0530

    initial commit

---
+++ reverse_of_sentence.java
@@ -0,0 +1,17 @@
+class reverse_of_sentence
+{public static void main(String args[])
+  {
+  
+   String a="MATRU is a good boy unfortunately";
+  
+   StringBuilder a1 = new StringBuilder(); 
+   
+   a1.append(a);
+
+   a1.reverse();
+  
+  System.out.println(a1);
+    
+  
+
+}}
