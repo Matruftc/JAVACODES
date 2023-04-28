8b8b196a0595b09578e746717672f624e032b9ca
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-28 23:39:50 +0530

    initial commit

---
+++ swap_of_two_numbers.java
@@ -0,0 +1,19 @@
+public class swap_of_two_numbers {
+public static void main(String args[])
+  {
+  
+    int a=5;
+    int b=10;
+    int c;
+ 
+  System.out.println("Values are"+a);
+  System.out.println("Values are"+b);
+
+   c=a;
+   a=b;
+   b=c;
+  System.out.println("Values are: " +a);
+  System.out.println("Values are: " +b);
+  System.out.println("Values are: " +c);
+}}
+
