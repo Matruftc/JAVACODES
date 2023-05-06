9bf58be74853d91b30a51ee51d9359431d4093cc
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-06 22:38:40 +0530

    initial commit

---
+++ reverse_of_array.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class reverse_of_array {
+public static void main(String args[])
+  {
+  int arr[]={12,34,23,12,2};
+
+
+  for(int i=4;i<arr.length;i--)
+  {
+   System.out.println(arr[i]);
+  }
+
+
+}}
+
