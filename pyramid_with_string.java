739bfb68f5c49fb51f98f48611291f1688f3fb52
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-06 22:41:39 +0530

    initial commit

---
+++ pyramid_with_string.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;  
+class pyramid_with_string{
+public static void main(String args[])
+  {
+  int arr[]={12,34,23,12,2,67};
+  
+  String stud[]={"max","Rek","parit","maert"};
+  
+  String a="priya";
+  char p;
+  String u="";
+
+  for(int i=0;i<a.length();i++)
+  {
+     
+     p=a.charAt(i);
+     u=u+p; 
+    
+  
+    System.out.println(u); 
+  }
+
+
+}}
+
