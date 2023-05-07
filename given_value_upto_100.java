e6741f29d2e09d380bbdbbdd91c7c14e169c50b9
Matru Prasad Mishra <matru19042003@gmail.com>
2023-05-07 17:08:01 +0530

    initial commit

---
+++ given_value_upto_100.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class given_value_upto_100 {
+public static void main(String args[])
+  {
+
+    Scanner obj=new Scanner(System.in);
+    int a = obj.nextInt();
+
+    System.out.println("value is:  "+a);
+ 
+    if(a%2!=0)
+    {
+        System.out.println("value is odd:  "+a);
+
+    }
+
+
+
+}}
+
