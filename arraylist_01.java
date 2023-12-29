5063bdd434473662b044bb9ae66dc468f602d3a9
Matru Prasad Mishra <matru19042003@gmail.com>
2023-12-29 10:38:35 +0530

    initial commit

---
+++ arraylist_01.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class arraylist_01 {
+    public static void main(String args[])
+    {
+        //create arraylist
+       ArrayList<String> lang = new ArrayList<>();
+       lang.add("C");
+       lang.add("Java");
+      lang.add("Python");
+
+       System.out.println(lang);
+    }
+}
