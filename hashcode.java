95195da50d1163ceb904240648022743384f4c02
Matru Prasad Mishra <matru19042003@gmail.com>
2023-12-29 10:47:56 +0530

    initial commit

---
+++ hashcode.java
@@ -0,0 +1,7 @@
+public class hashcode{
+    public static void main(String args[]) {
+    
+         String name="Priya";
+         System.out.println("Value in hashed: "+name.hashCode());
+    
+}}
