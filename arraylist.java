09619243a3ebdd2f2a211dc508870e0503152d0a
Matru Prasad Mishra <matru19042003@gmail.com>
2023-12-29 10:38:16 +0530

    initial commit

---
+++ arraylist.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class arraylist {
+    public static void main(String args[])
+    {
+        //create arraylist
+       ArrayList<Integer> num = new ArrayList<>();
+       num.add(1);
+       num.add(2);
+       num.add(3);
+
+       System.out.println(num);
+    }
+}
