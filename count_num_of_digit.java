f631c52cace2108629bba7cf38e79222f148968f
Matru Prasad Mishra <matru19042003@gmail.com>
2023-12-29 10:36:07 +0530

    initial commit

---
+++ count_num_of_digit.java
@@ -0,0 +1,18 @@
+public class count_num_of_digit {
+    
+    public static void main(String args[])
+    {
+    int a=454;
+
+    int res=0;
+    while(a != 0)
+    {
+       
+        a=a/10;
+        res++;
+        
+  
+    }
+       System.out.println(res);
+    
+    }}
