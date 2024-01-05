ef2ef0e84c9273d2fa6289ca0fe860bc0f01e663
Matru Prasad Mishra <matru19042003@gmail.com>
2024-01-05 20:23:22 +0530

    initial commit

---
+++ StringBuilder4.java
@@ -0,0 +1,9 @@
+ public class StringBuilder4 {
+    public static void main(String args[])
+    {
+        StringBuilder op = new StringBuilder("Hello");
+        op.delete(0,4);
+        System.out.println("Deletion  in the string: "+op);
+    }
+    
+}
