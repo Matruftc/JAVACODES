67fb86d85b295479c91872f5ae067c424d7adcb8
Matru Prasad Mishra <matru19042003@gmail.com>
2024-01-05 20:22:16 +0530

    initial commit

---
+++ StringBuilder0.java
@@ -0,0 +1,9 @@
+public class StringBuilder0 {
+    public static void main(String args[])
+    {
+        StringBuilder op = new StringBuilder("Hello");
+        op.append("Priya");
+        System.out.println("Append inthe string: "+op);
+    }
+    
+}
