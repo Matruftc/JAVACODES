90d760bc75fe0c7d01d0d73cfff572d3026ec20b
Matru Prasad Mishra <matru19042003@gmail.com>
2024-01-05 20:23:01 +0530

    initial commit

---
+++ StringBuilder3.java
@@ -0,0 +1,9 @@
+ public class StringBuilder3 {
+    public static void main(String args[])
+    {
+        StringBuilder op = new StringBuilder("Hello");
+        op.replace(0,5,"Priya");
+        System.out.println("Replace: "+op);
+    }
+    
+}
